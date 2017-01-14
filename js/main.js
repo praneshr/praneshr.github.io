943786eb88406175a61f3cc2c4a64fd66cc796d5
Pranesh Ravi <praneshpranesh@gmail.com>
2017-01-15 01:15:42 +0530

    Adding work info and a page loader

--- js/main.js
+++ js/main.js
@@ -3,20 +3,26 @@ var viewportHeight = window.innerHeight
 var viewportWidth = window.innerWidth
 
 var colorArray = [
-  '#3C3B5C',
   '#D53939',
-  '#616EEF',
-  '#7F4A88',
-  '#F6378F',
-  '#26C6D0',
   '#90D26D',
   '#FFEA85',
-  '#EA7362',
-  '#9E579D',
+  '#DBE2EF',
+  '#2A363B',
+  '#53354A',
 ]
 
+function randomString(len, charSet) {
+    charSet = charSet || 'abcdefghijklmnopqrstuiwxyz& ';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+}
+
 function setBannerViewPort() {
-  $('.banner').height(viewportHeight)
+  $('.vetti-scene').height(window.innerHeight)
 }
 
 function getRandom(min, max) {
@@ -30,8 +36,8 @@ function randomCircle() {
   }
   var id = (+new Date).toString(36) + Math.random().toString(36).slice(2, 7)
   circleArray.push(id)
-  $('.banner')
-    .append('<div class="circle" id="' + id + '"></div>')
+  $('.vetti-scene')
+    .append('<div class="rangoli-vattam" id="' + id + '"></div>')
   window.setTimeout(function () {
     const posX = getRandom(0, viewportWidth)
     const posY = getRandom(0, viewportHeight)
@@ -41,17 +47,27 @@ function randomCircle() {
       height: viewportHeight * 2,
       width: viewportHeight * 2,
       'background-color': colorArray[getRandom(0, colorArray.length - 1)],
-      transform: 'scale(3)',
+      transform: 'scale3d(3, 3, 3)',
     })
   }, 100)
 
   window.setTimeout(randomCircle, 10000)
 }
-
+function loading() {
+  var randomStr = randomString(12)
+  var randomStr2 = randomString(35)
+  $('.peru').text(randomStr)
+  $('.seira-vela').text(randomStr2)
+}
 $(document).ready(function () {
   setBannerViewPort()
 
   $(window).resize(setBannerViewPort)
-  window.setTimeout(randomCircle, 3000)
-
+  var id = window.setInterval(loading, 200)
+  window.setTimeout(function () {
+    window.clearInterval(id)
+    $('.peru').text('Pranesh Ravi')
+    $('.seira-vela').text('Web designer & Javascript Developer')
+    window.setTimeout(randomCircle, 3000)
+  }, 10000)
 })
